241560456cb80f1033a093a550da8a080f776b91
rothdu <rtdugmore@gmail.com>
2023-09-13 15:56:58 +0200

    made load from config code prettier

--- Simulation/simulate.py
+++ Simulation/simulate.py
@@ -13,19 +13,8 @@ def main():
     # loop over specified test parameters
     for test in config["tests"]:
 
-        #import global frequency
-        if test["frequency"]["global"]:
-            test["frequency"] = config["frequency"]
-        
-        # import global points array
-        if test["points"]["global"]:
-            test["points"] = config["points"]
-        elif test["points"]["random"]:
-            populate_random_points(test["points"], 0.8, 0.5)
-
-        # import global mic positions
-        if test["mics"]["global"]:
-            test["mics"] = config["mics"]
+        for key in test:
+            populate_test_from_global(config, test, key)
 
     
     with open("config_edit_test.json", "w") as write_file:
@@ -43,6 +32,10 @@ def add_point(points, max_x, max_y):
 def populate_random_points(points_dict, max_x, max_y):
     for i in range(points_dict["number"] - len(points_dict["points"])):
         add_point(points_dict["points"], max_x, max_y)
+
+def populate_test_from_global(config, test, key):
+    if test[key]["global"]:
+        test[key] = config[key]
 
 if __name__ == "__main__":
     main()
