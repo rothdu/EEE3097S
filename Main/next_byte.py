38750218ee112d0fa8bcc5044cc4552a2fc6b6a5
Simon Carthew <carthewsimone@gmail.com>
2023-10-03 14:49:31 +0200

    Change Next Byte

--- Main/next_byte.py
+++ Main/next_byte.py
@@ -2,13 +2,15 @@ import os
 import time
 import subprocess
 
+
 def inform_ready(pi_ip, pi_name):
-    source_file = "pc_ready.txt"
+    source_file = "Main/pc_ready.txt"
     destination_host = pi_name + "@" + pi_ip + ":/home/" + pi_name
 
     # Construct the scp command
     scp_command = ["scp", source_file, destination_host]
     subprocess.run(scp_command)
+
 
 def wait_trans(rpi1_fin_path, rpi2_fin_path):
     while not (os.path.exists(rpi1_fin_path) and os.path.exists(rpi2_fin_path)):
@@ -16,11 +18,13 @@ def wait_trans(rpi1_fin_path, rpi2_fin_path):
     os.remove(rpi1_fin_path)
     os.remove(rpi2_fin_path)
 
+
 def main():
     # Your main script logic here
-    inform_ready("192.168.137.132","rpi1")
-    wait_trans("rpi1_finnished.txt","rpi2_finnished.txt")
+    inform_ready("192.168.137.132", "rpi1")
+    wait_trans("Main/rpi1_finnished.txt", "Main/rpi2_finnished.txt")
     print("Transfer finnished")
+
 
 if __name__ == "__main__":
     main()
