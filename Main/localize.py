fd3c475faa8577a653fc04cee3c028e8e39dabe9
Si Teng Wu <wxxsit001@myuct.ac.za>
2023-10-04 16:39:07 +0200

    Update localize.py

--- Main/localize.py
+++ Main/localize.py
@@ -7,7 +7,13 @@ import gcc_phat
 from scipy import signal
 
 
-def localize(path1, path2, hyperbola="False"):
+def localize(path1, path2, micPos, hyperbola=False, refTDOA=False):
+    
+    returnDict = {
+        "results":[],
+        "hyperbola":[],
+        "reftdoa":[],
+    }
 
     # Import Wav files
     SR, rpi1 = wavfile.read(path1)
@@ -61,51 +67,58 @@ def localize(path1, path2, hyperbola="False"):
     tdoa_rpi2 = gcc_phat.gcc_phat(
         rpi2_chan_1, rpi2_chan_2, SR, max_tau)  # top left mic
     
-    tdoa_pisync = gcc_phat.gcc_phat(
-        rpi2_chan_1, rpi1_chan_1, SR, max_tau)  # top left mic
-    
-
-    print("tdoa_rpi1= " + str(tdoa_rpi1))
-    print("tdoa_rpi2= " + str(tdoa_rpi2))
-    print("tdoa_pisync= " + str(tdoa_pisync))
-
+    # print("tdoa_rpi1= " + str(tdoa_rpi1))
+    # print("tdoa_rpi2= " + str(tdoa_rpi2))
 
     if (tdoa_rpi1 == 0 or tdoa_rpi2 == 0):
-        return -10, -10
+        ans_arr = [-10,-10]
 
-    def function(variables):
-        (x, y) = variables
-        e1 = sqrt((x)**2+(y)**2) - sqrt((x-0.8)**2+(y)**2) - \
-            (tdoa_rpi1*constant.speed_of_sound)
-        e2 = sqrt((x)**2+(y)**2) - sqrt((x)**2+(y-0.5)**2) - \
-            (tdoa_rpi2*constant.speed_of_sound)
-        return [e1, e2]
-
-    ans_arr = [scipy.optimize.fsolve(function, (0, 0))]
-
-    # ans_arr = filter_ans(ans_arr)
-    if (hyperbola):
-
-        # defines a meshgrid of x and y, to produce meshgrids h_rpi1_test, h_rpi2_test for plotting
-        x, y = meshgrid(arange(0, 0.8, 0.8/100), arange(0, 0.5, 0.5/100))
-        # testing rpi 1
-        h_rpi1_test = sqrt((x)**2+(y)**2) - sqrt((x-0.8) **
-                                                 2+(y-0)**2) - (tdoa_rpi1*constant.speed_of_sound)
-        # testing rpi 2
-        h_rpi2_test = sqrt((x)**2+(y)**2) - sqrt((x-0)**2 +
-                                                 (y-0.5)**2) - (tdoa_rpi2*constant.speed_of_sound)
-
-        return ans_arr[0][0], ans_arr[0][1], x, y, h_rpi1_test, h_rpi2_test
     else:
-        return ans_arr[0][0], ans_arr[0][1]
+        def function(variables):
+            (x, y) = variables
+            e1 = sqrt((x-micPos[0][0])**2+(y-micPos[0][0])**2) - sqrt((x-micPos[1][0])**2+(y-micPos[1][0])**2) - \
+                (tdoa_rpi1*constant.speed_of_sound)
+            e2 = sqrt((x-micPos[0][0])**2+(y-micPos[0][0])**2) - sqrt((x-micPos[2][0])**2+(y-micPos[2][0])**2) - \
+                (tdoa_rpi2*constant.speed_of_sound)
+            return [e1, e2]
 
+        ans_arr = [scipy.optimize.fsolve(function, (0, 0))]
+        ans_arr = ans_arr[0]
+
+    if(ans_arr[0]<0 or ans_arr[0]>0.8 or ans_arr[1]<0 or ans_arr[1]>0.5):
+       ans_arr = [-10,-10] 
+
+    if hyperbola:
+        hyperbolas = genHyperbola(micPos, tdoa_rpi1, tdoa_rpi2)
+        returnDict["hyperbola"] += hyperbolas
+    
+    if refTDOA:
+        tdoa_pisync = gcc_phat.gcc_phat(rpi2_chan_1, rpi1_chan_1, SR, max_tau)
+        returnDict["reftdoa"].append(tdoa_pisync)
+    
+    # TODO: extract a "normal" array from the scipy optimise thingy
+    returnDict["results"] += ans_arr
+
+    return returnDict
+
+def genHyperbola(micPos, tdoa_rpi1, tdoa_rpi2):
+    # defines a meshgrid of x and y, to produce meshgrids h_rpi1_test, h_rpi2_test for plotting
+    x, y = meshgrid(arange(0, 0.8, 0.8/100), arange(0, 0.5, 0.5/100))
+    # testing rpi 1
+    h_rpi1_test = sqrt((x-micPos[0][0])**2+(y-micPos[0][0])**2) - sqrt((x-micPos[1][0])**2+(y-micPos[1][0])**2) - \
+        (tdoa_rpi1*constant.speed_of_sound)
+    # testing rpi 2
+    h_rpi2_test = sqrt((x-micPos[0][0])**2+(y-micPos[0][0])**2) - sqrt((x-micPos[2][0])**2+(y-micPos[2][0])**2) - \
+        (tdoa_rpi2*constant.speed_of_sound)
+    
+    return [x, y, h_rpi1_test, h_rpi2_test]
 
 def main():
-    x, y = localize("Main/bytes/rpi1_next_byte.wav",
-                    "Main/bytes/rpi2_next_byte.wav", False)
-    print(x)
-    print(y)
-
+    result = localize("Main/bytes/rpi1_next_byte.wav",
+                    "Main/bytes/rpi2_next_byte.wav",
+                    [[0,0.5],[0.8,0.5],[0.0,0.0]], False, False)
+    
+    print(result["results"])
 
 if __name__ == "__main__":
     main()
