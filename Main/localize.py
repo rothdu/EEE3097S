0ccbc05068263b6ccfd3d5b848b9caff3dbcae77
rothdu <rtdugmore@gmail.com>
2023-10-05 15:22:43 +0200

    changed to catch runtime warnings rather than exception

--- Main/localize.py
+++ Main/localize.py
@@ -4,6 +4,7 @@ import scipy.optimize
 import scipy.constants as constant
 from numpy import arange, meshgrid, sqrt
 import gcc_phat
+import warnings
 from scipy import signal
 from scipy.optimize import least_squares
 
@@ -51,18 +52,15 @@ def localize(path1, path2, micPos, startTime, hyperbola=False, refTDOA=False):
             e2 = sqrt((x-micPos[0][0])**2+(y-micPos[0][1])**2) - sqrt((x-micPos[2][0])**2+(y-micPos[2][1])**2) - \
                 (tdoa_rpi2*constant.speed_of_sound)
             return [e1, e2]
-        try:
-            ans_arr = scipy.optimize.fsolve(function, (0.4, 0.25))
-
-            if (ans_arr[0] < 0 or ans_arr[0] > 0.8 or ans_arr[1] < 0 or ans_arr[1] > 0.5):
-                Valid = False
-                returnDict["errorMessage"].append("Triangluation produced a value outside of the grid")
-
-        except:
-            print("An exception occurred")
-            Valid = False
-
         
+        ans_arr = scipy.optimize.fsolve(function, (0.4, 0.25))
+
+        if (ans_arr[0] < 0 or ans_arr[0] > 0.8 or ans_arr[1] < 0 or ans_arr[1] > 0.5):
+            Valid = False
+            returnDict["errorMessage"].append("Triangluation produced a value outside of the grid")
+
+        with warnings.catch_warnings(record=True) as w:
+            Valid = False
             returnDict["errorMessage"].append("Triangluation failed to find an intersection")
 
     if hyperbola and Valid:
